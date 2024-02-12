1401f28b881df9debbffffb065418303655fa6b9
taleenfahed <154735352+taleenfahed@users.noreply.github.com>
2024-02-12 17:19:07 +0300

    Add files via upload

---
+++ t.py
@@ -0,0 +1,17 @@
+def tal():
+    print("name:taleen fahed jaser alqutashat.")
+    print("age:18.")
+    print("date of birth:30-4-2005.")
+    print("religion: islam.")
+    print("education level:first year of university.")
+    print("academic specializion: artificial intelligence (AI).")
+    print("address: jordan-amman.")
+    print("hobbies: taekwondo,swimming,drawing,programming.")
+    print("studing at philadelphia university.")
+    
+def main():
+    tal()
+    
+    
+if__name__=="__main__":
+    main()        
